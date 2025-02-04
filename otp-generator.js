7825a341d5bb624c37c57f601afdfcbdf25644fa
KedarKulkarni-at-811412738807 <kedar.k@nexshore.com>
2025-02-04 23:13:19 +0530

    Added-OTP-generator-App

---
+++ otp-generator.js
@@ -0,0 +1,12 @@
+function generateOTP(length) {
+    const digits = '0123456789';
+    let otp = '';
+    for (let i = 0; i < length; i++) {
+        otp += digits[Math.floor(Math.random() * 10)];
+    }
+    return otp;
+}
+
+
+const otp = generateOTP(6);
+console.log('Generated OTP:', otp);
